683561116dd13bec58c0d4ada8f3891af61cfbd4
SouprP <potyrala.igor@gmail.com>
2024-10-08 21:06:07 +0200

    Setup

---
+++ src/main/java/me/souprpk/forumbackend/ForumBackendApplication.java
@@ -0,0 +1,13 @@
+package me.souprpk.forumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumBackendApplication.class, args);
+    }
+
+}
